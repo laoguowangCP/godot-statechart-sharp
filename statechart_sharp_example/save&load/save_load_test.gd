309eb28127fa84c6e65566e889fa617380a64dee
Peng Cheng <18019722385@163.com>
2024-07-21 21:52:37 +0800

    feat: save&load
    add test

--- statechart_sharp_example/save&load/save_load_test.gd
+++ statechart_sharp_example/save&load/save_load_test.gd
@@ -3,12 +3,15 @@ class_name StatechartSaveLoadTest
 extends Node
 
 
-var _statechart: Statechart
+var _statechart_1: Statechart
+var _statechart_2: Statechart
 var _snapshot: StatechartSnapshot
 
 
-func _ready():
-	_statechart = get_node_or_null("Statechart") as Statechart
-	_snapshot = _statechart.Save(true)
-	_statechart.Load(_snapshot, true, true)
+func _test():
+	_statechart_1 = get_node_or_null("Statechart1") as Statechart
+	_statechart_2 = get_node_or_null("Statechart2") as Statechart
+	_snapshot = _statechart_1.Save(true)
+	_statechart_2.Load(_snapshot, true, true)
+	print("Statechart configuration loaded.")
 
